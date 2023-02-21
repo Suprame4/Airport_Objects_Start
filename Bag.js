76427b0ebf4c61ea0a3a4ea87f73e579877df299
Suprame4 <adriancortesgonz@gmail.com>
2023-02-20 19:31:19 -0800

    Complete all test suites

--- Bag.js
+++ Bag.js
@@ -2,7 +2,27 @@ class Bag {
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
-  
-}
+    constructor(weight){ 
+ 
+
+        /*try{
+            //exception
+            if(!weight) throw new Error("bag needs weight")
+        }
+        catch(Error){
+            
+            return Error
+        }*/
+        if(!weight){
+            //throw an error if no weight is provided 
+            throw new Error("bag needs weight")
+        }
+
+        this.weight = weight
+    }
+
+
+        }
 
 //Export the module
+module.exports = Bag
--- Person.js
+++ Person.js
@@ -4,7 +4,17 @@ class Person {
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
+        constructor(name, bags){
+
+            this.name = name 
+            this.bags = []
+    
+        }    
     // Include an addBag method
+    addBag(bag){
+        this.bags.push(bag)
+    }
 }
 
 //Export the Module
+module.exports = Person
